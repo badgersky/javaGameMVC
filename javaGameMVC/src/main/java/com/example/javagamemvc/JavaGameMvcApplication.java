cc7f96b1d07421eb1b8d52edaac5604fe5820314
badgersky <szymon.pt4sznik@gmail.com>
2025-04-06 15:15:10 +0200

    project init

---
+++ javaGameMVC/src/main/java/com/example/javagamemvc/JavaGameMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagamemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGameMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGameMvcApplication.class, args);
+    }
+
+}
---
+++ javaGameMVC/src/test/java/com/example/javagamemvc/JavaGameMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javagamemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGameMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
