7402bf534e8e827378695abf176fa5f7d5dd7283
badgersky <szymon.pt4sznik@gmail.com>
2025-05-26 22:27:43 +0200

    created dataSeeder class to create admin user and correct account types if not present in database, added find by name method in accountType repository

--- javaGameMVC/src/main/java/com/example/javagamemvc/repository/AccountTypeRepository.java
+++ javaGameMVC/src/main/java/com/example/javagamemvc/repository/AccountTypeRepository.java
@@ -3,5 +3,8 @@ package com.example.javagamemvc.repository;
 import com.example.javagamemvc.entity.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountTypeRepository extends JpaRepository<AccountType, Long> {
+    Optional<AccountType> findByName(String name);
 }
